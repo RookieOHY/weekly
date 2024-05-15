dddef2581f2f6222097bc5c6aebf94200bd3172b
RookieOHY <204130199@qq.com>
2024-05-15 10:04:33 +0800

    删除推特

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,9 +12,9 @@ export function GET() {
   posts.splice(12);
 
   return rss({
-    title: '潮流周刊',
-    description: '记录工程师 Tw93 的不枯燥生活',
-    site: 'https://weekly.tw93.fun/',
+    title: '小欧周刊',
+    description: '记录工程师 RookieOHY 的不枯燥生活',
+    site: 'https://weekly.subhee.top/',
     customData: `<image><url>https://gw.alipayobjects.com/zos/k/qv/coffee-2-icon.png</url></image>`,
     items: posts.map((item) => {
       const url = item.url;
